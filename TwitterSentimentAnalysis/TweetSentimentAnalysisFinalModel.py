4c2178340c92f87ab3a63249466f9e282fbe6ff6
koolkhan2006 <koolkhan2006@gmail.com>
2020-03-07 13:16:29 +0530

    Final checkin

--- TwitterSentimentAnalysis/TweetSentimentAnalysisFinalModel.py
+++ TwitterSentimentAnalysis/TweetSentimentAnalysisFinalModel.py
@@ -3,7 +3,9 @@
 
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.naive_bayes import MultinomialNB, GaussianNB
 from sklearn.metrics import accuracy_score, f1_score, confusion_matrix, classification_report
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
@@ -13,7 +15,7 @@ import pickle
 import re
 import nltk
 nltk.download('wordnet')
-
+stopword = nltk.corpus.stopwords.words('english')
 train = pd.read_csv("train.csv", encoding = "Latin - 1")
 print(train.head())
 
@@ -42,109 +44,85 @@ print(train.isnull().sum())
 train = train.reset_index(drop=True)
 print(train.shape)
 
-print(train.shape)
-
-# lowercase of tweet column
-train["tweet"] = train["tweet"].str.lower()
-#checking datatype of tweet column
-print(train.dtypes)
-train['tweet'] = train['tweet'].astype(str)
-#changing column width to get full display
-pd.set_option('display.max_colwidth', -1)
 
 # emoticons funtion
 def convert_emoticons(text):
     for emoti in emot.EMOTICONS:
-        text = re.sub(u'('+emoti+')', "_".join(emot.EMOTICONS[emoti].replace(",","").split()), text)
+        text = re.sub(u'(' + emoti + ')', "_".join(emot.EMOTICONS[emoti].replace(",", "").split()), text)
     return text
-
-# remove emoticons
-train['tweet'] = train['tweet'].apply(lambda x : convert_emoticons(str(x)))
 
 # remove handles function
 def remove_pattern(input_txt, pattern):
     r = re.findall(pattern, input_txt)
     for i in r:
         input_txt = re.sub(i, '', input_txt)
-        
+
     return input_txt
 
-train['tweet'] = train['tweet'].astype(str)
-train['tweet'] = np.vectorize(remove_pattern)(train['tweet'], "@[\w]*") 
-train['tweet'] = np.vectorize(remove_pattern)(train['tweet'], "#[\w]*")
-
-#remove special characters and punctuations
+# remove special characters and punctuations
 def remove_punct(text):
-    text_nopunct="".join([char for char in text if char not in string.punctuation])
+    text_nopunct = "".join([char for char in text if char not in string.punctuation])
     return text_nopunct
 
-train['tweet'] = train['tweet'].astype(str)
-train['tweet'] = train['tweet'].apply(lambda x: re.sub(r'[^\w\s]',' ',x))
-train['tweet'] = train['tweet'].apply(lambda x: remove_punct(x))
-train['tweet'] = train['tweet'].str.replace("[^a-zA-Z#]", " ")
-
-#removing most important hashtags
-train['tweet']=train['tweet'].str.lower()
-train['tweet']=train['tweet'].str.replace("sxsw"," ")
-train['tweet']=train['tweet'].str.replace("link"," ")
-train['tweet']=train['tweet'].str.replace("quot"," ")
-train['tweet']=train['tweet'].str.replace("amp"," ")
-train['tweet']=train['tweet'].str.replace("google"," ")
-train['tweet']=train['tweet'].str.replace("ipad"," ")
-train['tweet']=train['tweet'].str.replace("iphone"," ")
-train['tweet']=train['tweet'].str.replace("apple"," ")
-train['tweet']=train['tweet'].str.replace("app"," ")
-train['tweet']=train['tweet'].str.replace("called"," ")
-train['tweet']=train['tweet'].str.replace("austin"," ")
-train['tweet']=train['tweet'].str.replace("store"," ")
-train['tweet']=train['tweet'].str.replace("android"," ")
-train['tweet']=train['tweet'].str.replace("pop"," ")
-train['tweet']=train['tweet'].str.replace("via"," ")
-train['tweet']=train['tweet'].str.replace("social"," ")
-train['tweet']=train['tweet'].str.replace("network"," ")
-train['tweet']=train['tweet'].str.replace("mention"," ")
-train['tweet']=train['tweet'].str.replace("new"," ")
-
-#remove words with less than 2 characters
-train['tweet'] = train['tweet'].apply(lambda x: ' '.join([w for w in x.split() if len(w)>2]))
-
-#tokenisation function
+# tokenisation function
 
 def tokenize(text):
-    tokens=re.split('\W+',text)
+    tokens = re.split('\W+', text)
     return tokens
 
-train['tweet']=train['tweet'].apply(lambda x:tokenize(x.lower()))
-
-# In[87]:
-stopword=nltk.corpus.stopwords.words('english')
-
-#remove stopwords
+# remove stopwords
 def remove_stopwords(tokenized_list):
-    text=[word for word in tokenized_list if word not in stopword]
+    text = [word for word in tokenized_list if word not in stopword]
     return text
-train['tweet']=train['tweet'].apply(lambda x:remove_stopwords(x))
 
-#lemmatizer function
-wn=nltk.WordNetLemmatizer()
+# lemmatizer function
+wn = nltk.WordNetLemmatizer()
 def lemmatizing(tokenized_text):
-    text=[wn.lemmatize(word) for word in tokenized_text]
+    text = [wn.lemmatize(word) for word in tokenized_text]
     return text
-
-train['tweet']=train['tweet'].apply(lambda x:lemmatizing(x))
-
-#Convert list to str
-train_string = pd.Series([' '.join(x) for x in train['tweet']])
-
-train['tweet'] = train_string
-
-# Word cloud for all words
-train['tweet'] = train['tweet'].astype(str)
 
 corpus = []
 for i in range(0,len(train)):
-    corpus.append(train['tweet'][i])
+    tweet_val = train["tweet"][i]
+    # lowercase of tweet column
+    tweet_val = tweet_val.lower()
+    # remove emoticons
+    tweet_val = convert_emoticons(str(tweet_val))
+    tweet_val = str(tweet_val)
+    tweet_val = np.vectorize(remove_pattern)(tweet_val, "@[\w]*")
+    tweet_val = np.vectorize(remove_pattern)(tweet_val, "#[\w]*")
+    tweet_val = str(tweet_val)
+    tweet_val = re.sub(r'[^\w\s]', ' ', tweet_val)
+    tweet_val = remove_punct(tweet_val)
+    tweet_val = tweet_val.replace("[^a-zA-Z#]", " ")
+    tweet_val = tweet_val.lower()
+    tweet_val = tweet_val.replace("sxsw", " ")
+    tweet_val = tweet_val.replace("link", " ")
+    tweet_val = tweet_val.replace("quot", " ")
+    tweet_val = tweet_val.replace("amp", " ")
+    tweet_val = tweet_val.replace("google", " ")
+    tweet_val = tweet_val.replace("ipad", " ")
+    tweet_val = tweet_val.replace("iphone", " ")
+    tweet_val = tweet_val.replace("apple", " ")
+    tweet_val = tweet_val.replace("app", " ")
+    tweet_val = tweet_val.replace("called", " ")
+    tweet_val = tweet_val.replace("austin", " ")
+    tweet_val = tweet_val.replace("store", " ")
+    tweet_val = tweet_val.replace("android", " ")
+    tweet_val = tweet_val.replace("pop", " ")
+    tweet_val = tweet_val.replace("via", " ")
+    tweet_val = tweet_val.replace("social", " ")
+    tweet_val = tweet_val.replace("network", " ")
+    tweet_val = tweet_val.replace("mention", " ")
+    tweet_val = tweet_val.replace("new", " ")
+    tweet_val = ' '.join([w for w in tweet_val.split() if len(w) > 2])
+    tweet_val = tokenize(tweet_val.lower())
+    tweet_val = remove_stopwords(tweet_val)
+    tweet_val = lemmatizing(tweet_val)
+    tweet_val = ' '.join(tweet_val)
+    corpus.append(tweet_val)
 
+print(len(corpus))
 # tfidf = TfidfVectorizer(max_features=5250)
 # X = tfidf.fit_transform(corpus).toarray()
 # pickle.dump(tfidf, open('tfidf.pkl','wb'))
@@ -160,17 +138,55 @@ print("Dividing the dataset into train and test")
 print("*"*50)
 X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.2, random_state = 42)
 
+# print("*"*50)
+# print("Apply Naiive Bayes classification")
+# print("*"*50)
+# nb = MultinomialNB(alpha=0.1)
+# nb.fit(X_train,y_train)
+# y_pred = nb.predict(X_test)
+# print(nb.score(X,y))
+# cm = confusion_matrix(y_test,y_pred)
+# cr = classification_report(y_test,y_pred)
+# print(cm)
+# print(cr)
+
+# print("*"*50)
+# print("Apply Naiive Bayes classification")
+# print("*"*50)
+# gaussian_nb = GaussianNB()
+# gaussian_nb.fit(X_train,y_train)
+# y_pred = gaussian_nb.predict(X_test)
+# print(gaussian_nb.score(X,y))
+# cm = confusion_matrix(y_test,y_pred)
+# cr = classification_report(y_test,y_pred)
+# print(cm)
+# print(cr)
+
 print("*"*50)
-print("Apply Naiive Bayes classification")
+print("Apply Logistic Regression")
 print("*"*50)
-nb = MultinomialNB()
-nb.fit(X_train,y_train)
-y_pred = nb.predict(X_test)
-print(nb.score(X,y))
+# log_reg = LogisticRegression(random_state=42,class_weight='balanced',C=1000,max_iter=500)
+log_reg = LogisticRegression(random_state=42)
+log_reg.fit(X_train,y_train)
+y_pred = log_reg.predict(X_test)
+print(log_reg.score(X,y))
 cm = confusion_matrix(y_test,y_pred)
 cr = classification_report(y_test,y_pred)
 print(cm)
 print(cr)
 
+# print("*"*50)
+# print("Executing Random Forest classifier")
+# print("*"*50)
+# rfc = RandomForestClassifier(random_state=42, class_weight = "balanced")
+# rfc.fit(X_train,y_train)
+# y_pred = rfc.predict(X_test)
+# rfc.score(X_test,y_test)
+# cm = confusion_matrix(y_test,y_pred)
+# cr = classification_report(y_test,y_pred)
+# print(rfc.score(X,y))
+# print(cm)
+# print(cr)
+
 # Saving model to disk
-pickle.dump(nb, open('TwitterSentimentAnalysisFinalModel.pkl','wb'))
+pickle.dump(log_reg, open('TwitterSentimentAnalysisFinalModel.pkl','wb'))
